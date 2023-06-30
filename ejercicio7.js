ed4cd150551794d81ea24d904471aff4795d09f5
unknown <Maxel26>
2023-06-30 18:04:59 -0500

    Se realizo ejercicio 7

---
+++ ejercicio7.js
@@ -0,0 +1,10 @@
+// 7. Valor: 0.25 - Convertir una cadena en una lista
+
+// Escriba una función para dividir una cadena y convertirla en una lista de palabras.
+
+function separarPalabras(str) {
+    let separar = str.split(' ');
+ return separar   
+}
+
+console.log(separarPalabras('Hola mi nombre es Axel Martinez'));
